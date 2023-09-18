aed91267e24db7a67900fdb9214ad97b6feed80a
basel.mariam <basel1948@hotmail.com>
2023-09-19 01:12:43 +0300

    add them & theme-rtl then fix issues

---
+++ .history/src/App_20230918193749.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useMemo } from "react";
+import { useLocation } from "react-router-dom";
+import logo from "./logo.svg";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [rtlCache, setRtlCache] = useState(null);
+  useMemo(() => {
+    const cacheRtl = createCache({
+      key: 'rtl',
+      stylisPlugins: [rtlPlugin],
+    });
+    setRtlCache(cacheRtl);
+  }, [])
+  // const { pathname } = useLocation();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20230919002009.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useMemo, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import logo from "./logo.svg";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import rtlPlugin from "stylis-plugin-rtl";
+
+function App() {
+  const [rtlCache, setRtlCache] = useState<any>(null);
+  const { pathname } = useLocation();
+
+  useMemo(() => {
+    const cacheRtl = createCache({
+      key: "rtl",
+      stylisPlugins: [rtlPlugin],
+    });
+    setRtlCache(cacheRtl);
+  }, []);
+  // const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement!.scrollTop = 0;
+    document.scrollingElement!.scrollTop = 0;
+  }, [pathname]);
+  
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+  );
+}
+
+export default App;
