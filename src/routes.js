780f4da4102701f42fae19fc9e6a51bc75019ab7
Tarek Ahmad <mhd.tarek.ahmad@gmail.com>
2023-10-07 21:04:19 +0300

    add the routes for home3, calendar, and protocols pages

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import Users from "./views/users/index.tsx";
 import SignIn from "./views/authentication/sign-in.tsx";
 import Dashboard from "./views/dashboard/index.tsx";
 import Dashboard2 from "./views/dashboard2/index.tsx";
+import { Protocols } from "./views/protocols";
+import { CalendarPage } from "./views/calendar";
+import { Dashboard3 } from "./views/dashboard3";
 
 const routes = [
   {
@@ -55,6 +58,36 @@ const routes = [
     icon: null, //<Icon fontSize="small">dashboard</Icon>,
     route: "/dashboard2",
     component: <Dashboard2 />, //<Dashboard />,
+    isPrivate: true,
+  },
+  {
+    type: "collapse",
+    name: "Home3",
+    ar_name: "الرئيسية 3",
+    key: "home3",
+    icon: null, //<Icon fontSize="small">dashboard</Icon>,
+    route: "/dashboard3",
+    component: <Dashboard3 />, //<Dashboard />,
+    isPrivate: true,
+  },
+  {
+    type: "collapse",
+    name: "calendar",
+    ar_name: "التقويم",
+    key: "calendar",
+    icon: null, //<Icon fontSize="small">dashboard</Icon>,
+    route: "/calendar",
+    component: <CalendarPage />, //<Dashboard />,
+    isPrivate: true,
+  },
+  {
+    type: "collapse",
+    name: "Protocols",
+    ar_name: "طلبات البروتوكول",
+    key: "protocols",
+    icon: null, //<Icon fontSize="small">dashboard</Icon>,
+    route: "/protocols",
+    component: <Protocols />, //<Dashboard />,
     isPrivate: true,
   },
 ];
